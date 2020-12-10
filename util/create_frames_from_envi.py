6bd6a0588197c4f7d4a5c08493067ac7051cb385
Winston Olson-Duvall <winstono@jpl.nasa.gov>
2020-12-10 15:07:10 -0800

    Tweak code to get it working

--- util/create_frames_from_envi.py
+++ util/create_frames_from_envi.py
@@ -13,18 +13,19 @@ import spectral.io.envi as envi
 def main():
 
     input_path = sys.argv[1]
-    start_line = sys.argv[2]
-    num_frames = sys.argv[3]
+    start_line = int(sys.argv[2])
+    num_frames = int(sys.argv[3])
 
     img = envi.open(input_path + ".hdr", input_path)
     meta = img.metadata.copy()
     meta["lines"] = 32
     line = start_line
     for frame_num in range(num_frames):
+        print("Working on frame number %i" % frame_num)
         frame = np.array(img[line:line + 32, :, :])
-        frame_path = "_".join(input_path, start_line, frame_num) + ".hdr"
-        envi.save_image(frame_path, frame, metadata=meta, ext="", force=True)
-
+        frame_path = "_".join([input_path, str(start_line), str(frame_num)])
+        envi.save_image(frame_path + ".hdr", frame, metadata=meta, interleave="bil", ext="", force=True)
+        line += 32
 
 if __name__ == '__main__':
     main()
