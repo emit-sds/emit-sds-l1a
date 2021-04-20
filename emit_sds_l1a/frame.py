7e2765ea4ac79ea19052d8cd525e4bdd752e5680
Winston Olson-Duvall <winston.olson-duvall@jpl.nasa.gov>
2021-04-20 08:09:43 -0700

    Adds frame_spoofer utility to create frames from compressed data files.  Updates Frame class to read in frames and write out compressed data files.

--- emit_sds_l1a/frame.py
+++ emit_sds_l1a/frame.py
@@ -1,33 +1,48 @@
 """
-The Frame class is used to read acquisition frames with uncompressed frame headers and compressed frame data.  This
-class can also be used to create artificial frames from compressed data by adding frame headers.
+The Frame class is used to read acquisition frames with uncompressed frame headers and compressed frame data.
 
 Author: Winston Olson-Duvall, winston.olson-duvall@jpl.nasa.gov
 """
 
+import csv
 import logging
+import os
 import sys
 
 logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s", level=logging.DEBUG)
+logger = logging.getLogger("emit-sds-l1a")
+
+HDR_NUM_BYTES = 1280
 
 
 class Frame:
 
     def __init__(self, frame_path=None):
-        hdr = None
-        data = None
-        # If we have a frame_path, then load it in to the Frame object
-        if frame_path:
-            hdr = self._construct_hdr(frame_path)
-            with open(frame_path, "rb") as f:
-                data = f.read()
+        logger.debug("Initializing frame from path %s" % frame_path)
+        with open(frame_path, "rb") as f:
+            frame = f.read()
+        self.hdr = frame[0: 1280]
+        data_size = int.from_bytes(self.hdr[4:8], byteorder="little", signed=False)
+        self.data = frame[1280: 1280 + data_size]
+        self.dcid = self.hdr[28:32].decode("utf-8")
+        self.frame_num = int.from_bytes(self.hdr[8:16], byteorder="little", signed=False)
 
-    def _construct_hdr(self, path):
-        logging.debug("Constructing hdr object")
-        hdr = bytearray()
-        sync_word = bytes.fromhex("81FFFF81")
-        logging.debug(sync_word)
-        hdr += sync_word
-        return hdr
+    def write_data(self, data_dir):
+        # Lookup start time using DCID
+        start_time_str = ""
+        csv_path = os.path.join(data_dir, "dcid_lookup.csv")
+        with open(csv_path, "r") as csvfile:
+            csvreader = csv.reader(csvfile)
+            fields = next(csvreader)
+            start_time_str = [row[1] for row in csvreader if row[0] == self.dcid][0]
 
-myframe = Frame(sys.argv[1])
+        fname = "_".join(["emit" + start_time_str, str(self.frame_num).zfill(2)])
+        out_path = os.path.join(data_dir, fname)
+        logger.debug("Writing data to path %s" % out_path)
+        with open(out_path, "wb") as f:
+            f.write(self.data)
+
+
+frame_path = sys.argv[1]
+frame = Frame(frame_path)
+frame.write_data(os.path.dirname(frame_path))
---
+++ util/frame_spoofer.py
@@ -0,0 +1,82 @@
+"""
+This utility takes a compressed 32-line frame and prepends an uncompressed, spoofed frame header to create a binary
+file similar to what is expected from the app FSF.
+
+Author: Winston Olson-Duvall, winston.olson-duvall@jpl.nasa.gov
+"""
+
+import logging
+import os
+import sys
+
+logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s", level=logging.DEBUG)
+logger = logging.getLogger("emit-sds-l1a")
+
+HDR_NUM_BYTES = 1280
+
+
+class FrameSpoofer:
+
+    def __init__(self, data_path=None):
+        self.hdr = None
+        self.data = None
+        # If we have a frame_path, then load it in to the Frame object
+        if data_path:
+            self.hdr = self._construct_hdr(data_path)
+            with open(data_path, "rb") as f:
+                self.data = f.read()
+                logger.debug("data length is %s" % len(self.data))
+                if len(self.data) % 16 > 0:
+                    self.data += bytearray(16 - len(self.data) % 16)
+                logger.debug("data length is now %s" % len(self.data))
+
+    def _construct_hdr(self, path):
+        logger.debug("Constructing hdr bytearray")
+        hdr = bytearray(HDR_NUM_BYTES)
+
+        # Add sync word
+        sync_word = bytes.fromhex("81FFFF81")
+        logger.debug(sync_word)
+        hdr[0:4] = sync_word
+        logger.debug(b"hdr[0:10]: %b" % hdr[:10])
+
+        # Add image size in bytes
+        size = os.path.getsize(path)
+        logger.debug("data size is %i" % size)
+        hdr[4:8] = size.to_bytes(4, byteorder="little", signed=False)
+
+        # Add frame number
+        frame_num = int(os.path.basename(path).split("_")[8].replace(".flex", ""))
+        logger.debug("frame_num is %i" % frame_num)
+        hdr[8:16] = frame_num.to_bytes(8, byteorder="little", signed=False)
+
+        # Get DCID from file name and write to hdr (as string for now)
+        dcid_str = os.path.basename(path).split("_")[0][-4:]
+        logger.debug("dcid_str: %s" % dcid_str)
+        dcid_str_arr = bytearray(dcid_str, "utf-8")
+        hdr[28:32] = dcid_str_arr
+
+        # Set collection status
+        if frame_num == 0:
+            status = 0
+
+
+        return hdr
+
+    def save(self, out_dir):
+        logger.debug("Saving frame to disk with frame header and compressed frame data")
+        dcid = self.hdr[28:32].decode("utf-8")
+        logger.debug("dcid is %s" % dcid)
+        frame_num = int.from_bytes(self.hdr[8:16], byteorder="little", signed=False)
+        fname = dcid + "_" + str(frame_num).zfill(2)
+        out_path = os.path.join(out_dir, fname)
+        binary = bytearray()
+        binary += self.hdr
+        binary += self.data
+        with open(out_path, "wb") as f:
+            f.write(binary)
+
+
+data_path = sys.argv[1]
+frame = FrameSpoofer(data_path)
+frame.save(os.path.dirname(data_path))
