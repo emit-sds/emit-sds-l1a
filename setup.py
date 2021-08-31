a83dfdd2d44b027f07c865d670fbb16626d02e9d
Winston Olson-Duvall <winston.olson-duvall@jpl.nasa.gov>
2021-08-31 14:27:51 -0700

    Update version to 0.2.0 for SDS release 2.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="emit_sds_l1a",
-    version="0.1.0",
+    version="0.2.0",
     author="Winston Olson-Duvall",
     author_email="winston.olson-duvall@jpl.nasa.gov",
     description="""
