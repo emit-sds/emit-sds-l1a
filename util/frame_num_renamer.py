0726f479431eec4a35770d59ab37b45e9294e339
Winston Olson-Duvall <winston.olson-duvall@jpl.nasa.gov>
2022-01-27 08:45:50 -0800

    Update frame number renamer script to work with current file naming

--- util/frame_num_renamer.py
+++ util/frame_num_renamer.py
@@ -12,8 +12,8 @@ correction = int(sys.argv[2])
 expected_num_frames = sys.argv[3].zfill(5)
 
 toks = frame_path.split("_")
-corrected_frame_num = str(int(toks[1]) + correction).zfill(5)
-out_path = "_".join([toks[0], corrected_frame_num, expected_num_frames, toks[3]])
+corrected_frame_num = str(int(toks[2]) + correction).zfill(5)
+out_path = "_".join([toks[0], toks[1], corrected_frame_num, expected_num_frames, toks[4], toks[5]])
 
 print(f"Renaming {frame_path} to {out_path}")
 os.rename(frame_path, out_path)
