37f5a314e622432c0413d84caa0c0fd9183f6cc9
Winston Olson-Duvall <winston.olson-duvall@jpl.nasa.gov>
2022-03-17 14:31:09 -0700

    Add raw waterfall script

---
+++ util/raw_waterfall.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 30 14:57:21 2022
+
+@author: bradley
+"""
+import os, sys
+import spectral.io.envi as envi
+from pathlib import Path
+import numpy as np
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import matplotlib.pyplot as plt
+
+filename = sys.argv[1]
+
+data_raw = envi.open(Path(filename).with_suffix('.hdr'))
+waterfall = np.mean(data_raw[:,:,:], axis=2)
+waterfall = waterfall / np.max(waterfall)
+
+plt.figure(figsize=(11, 8.5))
+plt.clf()
+im = plt.imshow(waterfall)
+plt.title('waterfall plot, average over spectral pixels')
+plt.xlabel('spatial pixel')
+plt.ylabel('frame')
+divider = make_axes_locatable(plt.gca())
+cax = divider.append_axes("right", size="5%", pad=0.05)
+plt.colorbar(im, cax=cax)
+plt.savefig(filename.replace('.img', '_waterfall.png'))
+plt.close()
